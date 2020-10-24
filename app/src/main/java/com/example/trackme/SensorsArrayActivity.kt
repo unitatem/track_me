fcd7d0564d150fe852acebffdb692ed26a8191ae
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2020-10-24 20:21:20 +0200

    Add AccelerometerActivity

--- app/src/main/java/com/example/trackme/SensorsArrayActivity.kt
+++ app/src/main/java/com/example/trackme/SensorsArrayActivity.kt
@@ -1,11 +1,18 @@
 package com.example.trackme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SensorsArrayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sensors_array)
+    }
+
+    fun onClickAccelerometerButton(view: View) {
+        val intent = Intent(this, AccelerometerActivity::class.java).apply {}
+        startActivity(intent)
     }
 }
