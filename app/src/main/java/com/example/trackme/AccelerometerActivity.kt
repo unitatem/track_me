55a68ed52206e347d55282f9a2ecf79e65fb046c
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2020-10-25 12:09:55 +0100

    Read and display accelerometer's measurements

--- app/src/main/java/com/example/trackme/AccelerometerActivity.kt
+++ app/src/main/java/com/example/trackme/AccelerometerActivity.kt
@@ -1,11 +1,51 @@
 package com.example.trackme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class AccelerometerActivity : AppCompatActivity() {
+class AccelerometerActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private var accSensor: Sensor? = null
+    private val logTag = "AccelerometerActivity"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_accelerometer)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event == null) return
+
+        findViewById<TextView>(R.id.acc_value_x).text = event.values[0].toString()
+        findViewById<TextView>(R.id.acc_value_y).text = event.values[1].toString()
+        findViewById<TextView>(R.id.acc_value_z).text = event.values[2].toString()
+    }
+
+    override fun onResume() {
+        Log.v(logTag, "onResume() >")
+        super.onResume()
+        accSensor?.also { sensor ->
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        Log.v(logTag, "onPause() >")
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 }
