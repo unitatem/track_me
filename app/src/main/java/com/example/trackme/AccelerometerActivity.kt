8bb0145956eed7b4ff588cd9a36b09fdbf00f119
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2020-12-03 00:16:24 +0100

    Add dummy chart to the AccelerometerActivity

--- app/src/main/java/com/example/trackme/AccelerometerActivity.kt
+++ app/src/main/java/com/example/trackme/AccelerometerActivity.kt
@@ -7,8 +7,14 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+
 
 class AccelerometerActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
@@ -22,6 +28,22 @@ class AccelerometerActivity : AppCompatActivity(), SensorEventListener {
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+
+        val chart = findViewById<View>(R.id.acc_chart) as LineChart
+
+        val entries: MutableList<Entry> = mutableListOf()
+        entries.add(Entry(0.0f, 0.0f))
+        entries.add(Entry(1.0f, 1.0f))
+        entries.add(Entry(2.0f, 0.0f))
+        entries.add(Entry(3.0f, -1.0f))
+        entries.add(Entry(4.0f, 0.0f))
+
+        val dataSet = LineDataSet(entries, "sin")
+
+        val lineData = LineData(dataSet)
+        chart.data = lineData
+        chart.invalidate()
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -30,9 +52,18 @@ class AccelerometerActivity : AppCompatActivity(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent?) {
         if (event == null) return
 
-        findViewById<TextView>(R.id.acc_value_x).text = getString(R.string.acc_x_val, event.values[0])
-        findViewById<TextView>(R.id.acc_value_y).text = getString(R.string.acc_x_val, event.values[1])
-        findViewById<TextView>(R.id.acc_value_z).text = getString(R.string.acc_x_val, event.values[2])
+        findViewById<TextView>(R.id.acc_value_x).text = getString(
+            R.string.acc_x_val,
+            event.values[0]
+        )
+        findViewById<TextView>(R.id.acc_value_y).text = getString(
+            R.string.acc_x_val,
+            event.values[1]
+        )
+        findViewById<TextView>(R.id.acc_value_z).text = getString(
+            R.string.acc_x_val,
+            event.values[2]
+        )
     }
 
     override fun onResume() {
