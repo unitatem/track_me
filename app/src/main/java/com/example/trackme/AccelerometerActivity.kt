b8c4a7ace02fd0fa28fb720104f4b985a0df2e58
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2020-12-04 23:23:58 +0100

    Draw 3 accelerometer axis on a chart

--- app/src/main/java/com/example/trackme/AccelerometerActivity.kt
+++ app/src/main/java/com/example/trackme/AccelerometerActivity.kt
@@ -1,7 +1,7 @@
 package com.example.trackme
 
 import android.content.Context
-import android.content.Intent
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -16,31 +16,51 @@ import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import kotlin.math.PI
-import kotlin.math.sin
 
 
 class AccelerometerActivity : AppCompatActivity(), SensorEventListener {
     private val TAG = "AccelerometerActivity"
 
-    private lateinit var sensorManager: SensorManager
-    private var accSensor: Sensor? = null
+    private lateinit var mSensorManager: SensorManager
+    private var mAccSensor: Sensor? = null
 
-    private var chart: LineChart? = null
+    private lateinit var mChart: LineChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_accelerometer)
 
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        initializeSensor()
+        initializeChart()
+    }
 
-        chart = findViewById<View>(R.id.acc_chart) as LineChart
+    private fun initializeSensor() {
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    }
 
+    private fun initializeChart() {
+        val lineData = LineData()
+        lineData.addDataSet(createDataSet("x", Color.RED))
+        lineData.addDataSet(createDataSet("y", Color.GREEN))
+        lineData.addDataSet(createDataSet("z", Color.BLUE))
+
+        mChart = findViewById<View>(R.id.acc_chart) as LineChart
+        mChart.data = lineData
+
+        val rightAxis: YAxis = mChart.axisRight
+        rightAxis.isEnabled = false
+    }
+
+    private fun createDataSet(label: String, color: Int): LineDataSet {
         val entries: MutableList<Entry> = mutableListOf()
-        val dataSet = LineDataSet(entries, "x")
-        val lineData = LineData(dataSet)
-        chart!!.data = lineData
+        val dataSet = LineDataSet(entries, label)
+
+        dataSet.setDrawValues(false)
+        dataSet.color = color
+        dataSet.setCircleColor(color)
+
+        return dataSet
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -49,6 +69,11 @@ class AccelerometerActivity : AppCompatActivity(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent?) {
         if (event == null) return
 
+        showSensorMeasurements(event)
+        displaySensorMeasurements(event)
+    }
+
+    private fun showSensorMeasurements(event: SensorEvent) {
         findViewById<TextView>(R.id.acc_value_x).text = getString(
             R.string.acc_x_val,
             event.values[0]
@@ -61,36 +86,35 @@ class AccelerometerActivity : AppCompatActivity(), SensorEventListener {
             R.string.acc_x_val,
             event.values[2]
         )
-
-        chart!!.data.getDataSetByLabel("x", false).addEntry(Entry(chart!!.data.entryCount.toFloat(), event.values[0]))
-
-        chart!!.data.notifyDataChanged()
-        chart!!.notifyDataSetChanged()
-
-        chart!!.setVisibleXRangeMaximum(50.0f);
-        chart!!.moveViewToX(chart!!.data.entryCount.toFloat());
     }
 
-    // TODO remove later
-    fun onClickButton(view: View) {
-        chart!!.data.getDataSetByLabel("x", false).addEntry(Entry(chart!!.data.entryCount.toFloat(), 1.0f))
+    private fun displaySensorMeasurements(event: SensorEvent) {
+        val entriesCnt = mChart.data.entryCount.toFloat()
+        mChart.data.getDataSetByLabel("x", false)
+            .addEntry(Entry(entriesCnt, event.values[0]))
+        mChart.data.getDataSetByLabel("y", false)
+            .addEntry(Entry(entriesCnt, event.values[1]))
+        mChart.data.getDataSetByLabel("z", false)
+            .addEntry(Entry(entriesCnt, event.values[2]))
 
-        chart!!.data.notifyDataChanged()
-        chart!!.notifyDataSetChanged()
-        chart!!.invalidate();
+        mChart.data.notifyDataChanged()
+        mChart.notifyDataSetChanged()
+
+        mChart.setVisibleXRangeMaximum(50.0f);
+        mChart.moveViewToX(entriesCnt);
     }
 
     override fun onResume() {
         Log.v(TAG, "onResume() >")
         super.onResume()
-        accSensor?.also { sensor ->
-            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+        mAccSensor?.also { sensor ->
+            mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME)
         }
     }
 
     override fun onPause() {
         Log.v(TAG, "onPause() >")
         super.onPause()
-        sensorManager.unregisterListener(this)
+        mSensorManager.unregisterListener(this)
     }
 }
