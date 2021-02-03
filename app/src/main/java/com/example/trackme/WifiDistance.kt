911a96760c5abe577944d58b651298b24b464bd7
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-02-04 00:06:44 +0100

    Implement equation for Wifi distance

--- app/src/main/java/com/example/trackme/WifiDistance.kt
+++ app/src/main/java/com/example/trackme/WifiDistance.kt
@@ -3,14 +3,14 @@ package com.example.trackme
 import kotlin.math.log10
 import kotlin.math.pow
 
-// https://stackoverflow.com/questions/21850721/best-method-to-determin-distance-with-wi-fi-signal-strength
-// https://journals.sagepub.com/doi/full/10.1155/2014/371350
 class WifiDistance {
     fun getDistance(rssi: Double, frequency: Double): Double {
-        return freeSpacePathLoss(rssi, frequency)
+        return stackoverflowEquation(rssi)
     }
 
     // TODO
+    // https://stackoverflow.com/questions/21850721/best-method-to-determin-distance-with-wi-fi-signal-strength
+    // https://journals.sagepub.com/doi/full/10.1155/2014/371350
     private fun freeSpacePathLoss(rssi: Double, frequency: Double): Double {
         // for distance in meters, strength in dB, frequency in MHz
         val k = 27.55
@@ -18,6 +18,15 @@ class WifiDistance {
 
         val d = 10.0.pow((rssi - 20.0 * log10(frequency_MHz) - k) / 20.0)
         return d
+    }
+
+    // https://electronics.stackexchange.com/questions/83354/calculate-distance-from-rssi
+    private fun stackoverflowEquation(rssi: Double): Double {
+        val referenceRssi = -30 // received signal strength in dBm at 1 metre
+        val propagationConstant = 2 // also path-loss exponent
+
+        val distance = 10.0.pow((referenceRssi - rssi) / (10.0 * propagationConstant))
+        return distance
     }
 
     companion object {
