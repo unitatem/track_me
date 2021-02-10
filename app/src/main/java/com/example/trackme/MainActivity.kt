f0caf015222e0540a2a47415a7f799bf7e094b4c
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-02-11 00:01:25 +0100

    Remove DisplayMessageActivity

--- app/src/main/java/com/example/trackme/MainActivity.kt
+++ app/src/main/java/com/example/trackme/MainActivity.kt
@@ -3,24 +3,12 @@ package com.example.trackme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
-import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    // Called when the user taps the Send button
-    fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.editText)
-        val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
-        }
-        startActivity(intent)
     }
 
     fun onClickSensorsArrayButton(view: View) {
